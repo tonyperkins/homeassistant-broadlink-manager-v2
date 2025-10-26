59d8dd3fdfa1025444235de71995c8d380e0de20
Tony Perkins <perkins.tony@gmail.com>
2025-10-26 12:02:08 -0500

    Increase delay to 2 seconds for integration_only untracked detection

--- frontend/src/components/commands/CommandLearner.vue
+++ frontend/src/components/commands/CommandLearner.vue
@@ -621,10 +621,10 @@ const startLearning = async () => {
       
       // Always reload untracked commands to get current state from server
       // This will show integration_only commands as untracked
-      // For integration_only, add a small delay to allow storage file to update
+      // For integration_only, add a delay to allow storage file to update
       if (saveDestination.value === 'integration_only') {
-        // Wait a bit for HA to write to storage file
-        await new Promise(resolve => setTimeout(resolve, 1000))
+        // Wait for HA to write to storage file (can take 10+ seconds in standalone mode)
+        await new Promise(resolve => setTimeout(resolve, 2000))
       }
       await loadUntrackedCommands()
       
